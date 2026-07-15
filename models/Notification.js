79e72eca718901e2725f36e1eedaa7bc59fbba40
MR_DIV <vshntvelip@gmail.com>
2026-07-15 11:48:57 +0530

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -2,35 +2,35 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const NotificationSchema = new Schema({
-    recipient: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true
-    },
-    
-    type: {
-        type: String,
-        enum: ["comment", "reply", "like", "follow", "mention"],
-        required: true
-    },
-    
-    title: String,
-    message: String,
-    
-    // Related entities
-    blog: {
-        type: Schema.Types.ObjectId,
-        ref: "blog"
-    },
-    
-    actor: {
-        type: Schema.Types.ObjectId,
-        ref: "user"
-    },
-    
-    // Status
-    isRead: { type: Boolean, default: false },
-    
+  recipient: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true
+  },
+
+  type: {
+    type: String,
+    enum: ["blogPost", "comment", "reply", "like", "follow", "mention"],
+    required: true
+  },
+
+  title: String,
+  message: String,
+
+  // Related entities
+  blog: {
+    type: Schema.Types.ObjectId,
+    ref: "blog"
+  },
+
+  actor: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+
+  // Status
+  isRead: { type: Boolean, default: false },
+
 }, { timestamps: true });
 
 // Index for performance
@@ -39,4 +39,3 @@ NotificationSchema.index({ createdAt: -1 });
 
 const Notification = mongoose.models.Notification || model("Notification", NotificationSchema);
 module.exports = Notification;
-
